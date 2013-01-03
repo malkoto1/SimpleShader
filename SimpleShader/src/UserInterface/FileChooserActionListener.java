15997a9b97691dd7ef748a2c4b3f60f662d216b9
malkoto1 <rasparuhov@gmail.com>
2013-01-03 18:23:48 +0200

    Dali?

---
+++ SimpleShader/src/UserInterface/FileChooserActionListener.java
@@ -0,0 +1,20 @@
+package UserInterface;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+
+import javax.swing.JFileChooser;
+
+public class FileChooserActionListener implements ActionListener {
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		
+		System.out.println("EBIS  I MAIKATA");
+		
+//		final JFileChooser fc = new JFileChooser();
+//		fc.showOpenDialog(fc);
+//		SwingApp.inputImage = fc.getSelectedFile();
+	}
+}
--- SimpleShader/src/UserInterface/SwingApp.java
+++ SimpleShader/src/UserInterface/SwingApp.java
@@ -14,56 +14,68 @@ import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 
 public class SwingApp {
-    public static void main(String[] args) throws IOException {
-        final Image image = ImageIO.read(new File("resources/background.png"));
-        final JFrame frame = new JFrame();
-        frame.add(new ImagePanel(image));
-        frame.setSize(700, 700);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
-        JMenuBar menuBar = new JMenuBar();
-        frame.setJMenuBar(menuBar);
+	public static File inputImage;
 
+	public static void main(String[] args) throws IOException {
+		final Image image = ImageIO.read(new File("resources/background.png"));
+		final JFrame frame = new JFrame();
+		frame.add(new ImagePanel(image));
+		frame.setSize(700, 700);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        // File Menu, F - Mnemonic
-        JMenu fileMenu = new JMenu("File");
-        fileMenu.setMnemonic(KeyEvent.VK_F);
-        menuBar.add(fileMenu);
+		JMenuBar menuBar = new JMenuBar();
+		frame.setJMenuBar(menuBar);
 
-        // File->New, N - Mnemonic
-        JMenuItem newImageMenuItem = new JMenuItem("New Image", KeyEvent.VK_N);
-        newImageMenuItem.addActionListener(new MyActionListener());
-        fileMenu.add(newImageMenuItem);
-        
-        frame.setVisible(true);
-    }
+		// File Menu, F - Mnemonic
+		JMenu fileMenu = new JMenu("File");
+		fileMenu.setMnemonic(KeyEvent.VK_F);
+		menuBar.add(fileMenu);
+
+		// File->New, N - Mnemonic
+		JMenuItem newImageMenuItem = new JMenuItem("New Image", KeyEvent.VK_N);
+		newImageMenuItem.addActionListener(new FileChooserActionListener());
+		fileMenu.add(newImageMenuItem);
+
+		if (inputImage != null) {
+			Image loadedImage = ImageIO.read(inputImage);
+			ImagePanel panel = new ImagePanel(loadedImage);
+			panel.setSize(frame.getHeight() - 100, frame.getWidth() - 100);
+			frame.add(panel);
+			frame.repaint();
+		}
+		else {
+			
+		}
+
+		frame.setVisible(true);
+	}
 }
 
 @SuppressWarnings("serial")
 class ImagePanel extends JPanel {
-    private Image image;
-    private boolean tile;
+	private Image image;
+	private boolean tile;
 
-    ImagePanel(Image image) {
-        this.image = image;
-        this.tile = false;
-    };
+	ImagePanel(Image image) {
+		this.image = image;
+		this.tile = false;
+	};
 
-    @Override
-    public void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        if (tile) {
-            int iw = image.getWidth(this);
-            int ih = image.getHeight(this);
-            if (iw > 0 && ih > 0) {
-                for (int x = 0; x < getWidth(); x += iw) {
-                    for (int y = 0; y < getHeight(); y += ih) {
-                        g.drawImage(image, x, y, iw, ih, this);
-                    }
-                }
-            }
-        } else {
-            g.drawImage(image, 0, 0, getWidth(), getHeight(), this);
-        }
-    }
+	@Override
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		if (tile) {
+			int iw = image.getWidth(this);
+			int ih = image.getHeight(this);
+			if (iw > 0 && ih > 0) {
+				for (int x = 0; x < getWidth(); x += iw) {
+					for (int y = 0; y < getHeight(); y += ih) {
+						g.drawImage(image, x, y, iw, ih, this);
+					}
+				}
+			}
+		} else {
+			g.drawImage(image, 0, 0, getWidth(), getHeight(), this);
+		}
+	}
 }
